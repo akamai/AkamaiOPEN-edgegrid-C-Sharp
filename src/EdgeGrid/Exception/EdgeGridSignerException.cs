708369e0e3184b1d385c564b2f9c5e55e5e3b4db
migueloktaio <miguelc@oktana.io>
2018-04-17 17:47:43 -0300

    removed old files

--- src/EdgeGrid/Exception/EdgeGridSignerException.cs
+++
@@ -1,9 +0,0 @@
-namespace Akamai.EdgeGrid.Exception
-{
-    public class EdgeGridSignerException : System.Exception
-    {
-        public EdgeGridSignerException(){}
-        public EdgeGridSignerException(string message) : base(message){}
-        public EdgeGridSignerException(string message, System.Exception inner): base(message, inner){} 
-    }
-}
