6353445d044889762d81a69401ce6d126d40b6b2
migueloktaio <miguelc@oktana.io>
2018-04-17 19:35:45 -0300

    Added general solution and project for the unit tests

---
+++ EdgeGrid-Signer-httpclient-unit-tests/EdgeGridSignerTest.cs
@@ -0,0 +1,20 @@
+using NUnit.Framework;
+
+namespace Tests
+{
+    public class EdgeGridSignerTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+            System.Diagnostics.Debugger.Launch();
+            string hello = "";
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
