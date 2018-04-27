b25651ce2e13fd78ccdfe8394e756a22d47cc602
Guillermo Figueroa <guillermof@oktana.io>
2018-04-27 16:06:45 -0300

    Adding timestamp class and nonce

---
+++ EdgeGrid-Signer-httpclient/src/EdgeGrid/Authentication/EdgeGridTimesNonce.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace AkamaiEdgeGrid.EdgeGrid
+{
+    /// <summary>
+    /// Generate a nonce.
+    /// </summary>
+    public class EdgeGridNonce
+    {
+        protected string nonce;
+
+        public EdgeGridNonce()
+        {
+            this.nonce = this.getNonce();
+        }
+
+        public EdgeGridNonce(string nonce)
+        {
+            if (String.IsNullOrWhiteSpace(nonce))
+            {
+                this.nonce = this.getNonce();
+            }
+            else
+            {
+                this.nonce = nonce;
+            }
+        }
+
+        public string getNonce(){
+            return Guid.NewGuid().ToString();
+        }
+
+        public override string ToString()
+        {
+            return this.nonce;
+        }
+    }
+}
---
+++ EdgeGrid-Signer-httpclient/src/EdgeGrid/Authentication/EdgeGridTimestamp.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Globalization;
+
+namespace AkamaiEdgeGrid.EdgeGrid
+{
+    public class EdgeGridTimestamp
+    {
+
+        private const string FORMAT = "yyyyMMddTHH:mm:ss+0000";
+        private TimeSpan timestampInterval;
+        private DateTime timestamp;
+
+        public EdgeGridTimestamp()
+        {
+            this.timestamp = DateTime.UtcNow;
+            this.timestampInterval = new TimeSpan(0, 0, 10);
+        }
+
+        public EdgeGridTimestamp(string timestamp)
+        {
+            DateTime dateValue;
+            if(DateTime.TryParseExact(timestamp, FORMAT, null, DateTimeStyles.None, out dateValue))
+            {
+                this.timestamp = dateValue;
+                this.timestampInterval = new TimeSpan(0, 0, 10);
+            }
+            else
+            {
+                throw new ArgumentException("Invalid timestamp format");
+            }
+        }
+
+        public TimeSpan TimestampInterval
+        {
+            get => this.timestampInterval;
+            set
+            {
+                if (value == null)
+                {
+                    this.timestampInterval = new TimeSpan(0, 0, 10);
+                }
+                else
+                {
+                    this.timestampInterval = value;
+                }
+            }
+        }
+
+        public DateTime Timestamp
+        {
+            get => this.timestamp;
+            set => this.timestamp = value;
+        }
+
+        /// <summary>
+        /// Determinate if the Timestamp is valid
+        /// </summary>
+        /// <returns><c>true</c>, if valid timestamp, <c>false</c> otherwise.</returns>
+        public bool isValid()
+        {
+            bool result = false;
+
+            DateTime now = DateTime.UtcNow;
+            result = this.timestamp.Add(this.timestampInterval) >= now;
+
+            return result;
+        }
+
+        /// <summary>
+        /// Sets the valid interval
+        /// </summary>
+        /// <param name="timespanInterval">Timespan interval</param>
+        public void setValidFor(TimeSpan timespanInterval)
+        {
+            this.timestampInterval = timespanInterval;
+        }
+
+        public override string ToString()
+        {
+            return this.timestamp.ToString(FORMAT);
+        }
+    }
+}
