2a2e95aebd5d2e8ecc6f1288b9a6d99c2fa12a39
Guillermo Figueroa <guillermof@oktana.io>
2018-05-03 16:57:26 -0300

    * Adding test project for OpenApi program * Refactoring and cleaning the code

---
+++ OpenApi/Exception/OpenApiArgumentException.cs
@@ -0,0 +1,8 @@
+﻿namespace OpenApi.Exception
+{
+    public class OpenApiArgumentException : System.Exception
+    {
+        public OpenApiArgumentException(string message) : base(message){}
+        public OpenApiArgumentException(string message, System.Exception inner): base(message, inner){} 
+    }
+}
