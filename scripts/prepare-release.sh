daa34f0f6689395409c15ec410635c139b806557
Michal Wojcik <miwojci@akamai.com>
2025-12-18 11:18:32 +0100

    DXE-6056 Prepare RC 1.0.0-preview

--- scripts/prepare-release.sh
+++ scripts/prepare-release.sh
@@ -6,7 +6,7 @@
 set -e
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-PROJECT_ROOT="$SCRIPT_DIR"
+PROJECT_ROOT="$(dirname "$SCRIPT_DIR")"
 PACKAGE_DIR="$PROJECT_ROOT/nupkg"
 
 echo "=========================================="
